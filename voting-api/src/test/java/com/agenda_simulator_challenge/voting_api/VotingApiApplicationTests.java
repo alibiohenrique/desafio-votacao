6e02fbda59989e9c2db55f00379b4d43aff8edb6
alibiohenrique <alibio.henrique@gmail.com>
2025-01-26 16:39:48 -0300

    feat: add initial Spring Boot dependencies

---
+++ voting-api/src/test/java/com/agenda_simulator_challenge/voting_api/VotingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenda_simulator_challenge.voting_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
