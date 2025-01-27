efd9082b690360edb9af53bc2029cae31c42caed
alibiohenrique <alibio.henrique@gmail.com>
2025-01-27 17:13:53 -0300

    feat: creating test case for controllers  in agenda, voting-session and vote

---
+++ voting-api/src/test/java/com/agenda_simulator_challenge/voting_api/controller/AgendaControllerTest.java
@@ -0,0 +1,12 @@
+package com.agenda_simulator_challenge.voting_api.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AgendaControllerTest {
+
+    @Test
+    void createAgenda() {
+    }
+}
